926392b5f5ef573dab668f434b4a2229ef6aa8a4
Muhammed Saied <bensa3ed.m@gmail.com>
2017-11-03 16:29:39 +0200

    Add files via upload

---
+++ mysite/js/myscripts.js
@@ -0,0 +1,9 @@
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip();
+});
+
+$(document).ready(function(){
+  $('.carousel').carousel({interval: 6000});
+  });
+
+$(function(){$('[data-toggle="popover"]').popover();});
